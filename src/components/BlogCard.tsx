68721efa335f407e20f486aa986e9bea8420a169
Sapar Sayat <saparsayat01@gmail.com>
2024-08-23 22:11:51 +0500

    feat: blog card fix

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -14,18 +14,24 @@ interface BlogCardProps {
 const BlogCard: React.FC<BlogCardProps> = ({ id, title, thumbnail, tags }) => {
   return (
     <Link href={`/posts/${id}`} className="block">
-      <div className="bg-white rounded-lg shadow-md overflow-hidden transition-shadow duration-300 ease-in-out hover:shadow-lg rounded-xl">
+      <div className="bg-white shadow-md overflow-hidden transition-shadow duration-300 ease-in-out hover:shadow-lg rounded-xl">
         <div className="relative w-full h-48">
-          <Image
-            src={thumbnail as string}
-            alt={title}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-            className="rounded-t-lg"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            priority
-          />
+          {thumbnail ? (
+            <Image
+              src={thumbnail}
+              alt={title}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              className="rounded-t-lg"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              priority
+            />
+          ) : (
+            <div className="bg-gray-200 flex items-center justify-center h-full rounded-t-lg">
+              <span className="text-gray-500">No Image Available</span>
+            </div>
+          )}
         </div>
         <div className="p-6">
           <h2 className="text-2xl font-bold mb-2">{title}</h2>
